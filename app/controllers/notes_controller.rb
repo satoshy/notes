cc5f1b85e1c05fad989b89a5584f57e08ecae827
satoshy <alex.stipanenko@gmail.com>
2015-10-17 16:48:18 +0300

    user login form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,28 +1,21 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-  # GET /notes
-  # GET /notes.json
   def index
     @notes = Note.all
   end
 
-  # GET /notes/1
-  # GET /notes/1.json
   def show
   end
 
-  # GET /notes/new
   def new
     @note = Note.new
   end
 
-  # GET /notes/1/edit
   def edit
   end
 
-  # POST /notes
-  # POST /notes.json
   def create
     @note = Note.new(note_params)
 
@@ -37,8 +30,6 @@ class NotesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
@@ -51,8 +42,6 @@ class NotesController < ApplicationController
     end
   end
 
-  # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class NotesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_note
       @note = Note.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
       params.require(:note).permit(:title, :body)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:login]
+  
+  validates :username,:presence => true,
+            :uniqueness => {:case_sensitive => false}, 
+            length: {maximum: 255}
+
+  validates_format_of :username, with: /\A[a-zA-Z0-9]*\z/
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
